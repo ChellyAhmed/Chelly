3d93910e1c019b10b759c89f7b6b8bd55934c98c
ChellyAhmed <ahmed.chelly@medtech.tn>
2023-04-02 14:16:23 +0100

    Home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,33 @@
-function Home(){
-    return(
+//import bootstrap icons:
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+function Home() {
+    return (
         <div className="full-page-div" >
             <h1 className="smile" >^_^</h1>
             <p>
-                Hi! <br/>
-                I'm Ahmed Chelly. <br/>
+                Hi! <br />
+                I'm Ahmed Chelly. <br />
                 I'm a software engineer and a UX designer.
             </p>
+
+            <div className="icons" >
+                <a href="#">
+                    <i className="bi bi-linkedin"/>
+                </a>
+                <a href="#">
+                    <i className="bi bi-behance" />
+                </a>
+                <a href="#">
+                    <i className="bi bi-github"/>
+                </a>
+                <a href="#">
+                    <i className="bi bi-whatsapp"/>
+                </a>
+                <a href="#">
+                    <i className="bi bi-envelope-at-fill"/>
+                </a>
+            </div>
 
         </div>
     );
