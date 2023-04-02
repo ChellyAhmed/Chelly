be2ea101cafb753e25b3a72efc347e909c7267c4
ChellyAhmed <ahmed.chelly@medtech.tn>
2023-04-02 14:35:06 +0100

    Started BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import Home from './screens/Home';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <h1>Error</h1>,
+  },
+  {
+    path: "/About",
+    element: <Home />,
+  },
+]);
 
 function App() {
   console.log("Hello World!")
   return (
-   <Home/>
+    <RouterProvider router={router} />
   );
 }
 
