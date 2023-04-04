23306548ded671ec5c5e6bc59c5cd92e2c226c8a
ChellyAhmed <ahmed.chelly@medtech.tn>
2023-04-05 00:27:22 +0100

    Updated IconsBar for accessibility

--- src/components/IconsBar.js
+++ src/components/IconsBar.js
@@ -3,19 +3,19 @@ import '../styles/dist/iconsBar.css'
 function IconsBar() {
     return (
         <div className="icons" >
-            <a href="https://www.linkedin.com/in/ahmed-chelly-912054153/" target="_blank" rel="noreferrer">
+            <a href="https://www.linkedin.com/in/ahmed-chelly-912054153/" target="_blank" rel="noreferrer" aria-label="linkedin">
                 <i className="bi bi-linkedin" />
             </a>
-            <a href="https://www.behance.net/Ahmedchelly" target="_blank" rel="noreferrer">
+            <a href="https://www.behance.net/Ahmedchelly" target="_blank" rel="noreferrer" aria-label="behance">
                 <i className="bi bi-behance" />
             </a>
-            <a href="https://github.com/ChellyAhmed" target="_blank" rel="noreferrer">
+            <a href="https://github.com/ChellyAhmed" target="_blank" rel="noreferrer" aria-label="github">
                 <i className="bi bi-github" />
             </a>
-            <a href="https://wa.me/0021697556892" target="_blank" rel="noreferrer">
+            <a href="https://wa.me/0021697556892" target="_blank" rel="noreferrer" aria-label="whatsapp">
                 <i className="bi bi-whatsapp" />
             </a>
-            <a href="mailto:ahmed.chelly@medtech.tn" target="_blank" rel="noreferrer">
+            <a href="mailto:ahmed.chelly@medtech.tn" target="_blank" rel="noreferrer" aria-label="envelope">
                 <i className="bi bi-envelope-at-fill" />
             </a>
         </div>
